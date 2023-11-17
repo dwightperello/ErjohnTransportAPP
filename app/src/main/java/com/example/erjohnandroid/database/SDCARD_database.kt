e18fedf435509d6028cff350c5e48cf6d62816b8
Dwight <dwight@easypos.com.ph>
2023-11-17 09:38:05 +0800

    deploy

--- app/src/main/java/com/example/erjohnandroid/database/SDCARD_database.kt
+++ app/src/main/java/com/example/erjohnandroid/database/SDCARD_database.kt
@@ -20,7 +20,7 @@ abstract class SDCARD_database:RoomDatabase() {
       //val dbPath = File("/data/data/com.example.erjohnandroid/files", "CopyErjohnDB.db")
         // val dbPath = File("/sdcard/Documents/files","CopyErjohnDB.db")
       val dbPath = File("/storage/CC18-5A74/Documents/files","CopyErjohnDB.db")
-      // val dbPath = File("/sdcard/Documents/sdfiles","CopyErjohnDB.db")
+       //val dbPath = File("/sdcard/Documents/sdfiles","CopyErjohnDB.db")
 
       //  val dbPath = File("/sdcard", "CopyErjohnDB.db")
         fun getAppDB(context: Context):SDCARD_database{
